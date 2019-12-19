75e81029aa2370455bc8579451813c629239ae59
jorpac <36743869+jorpac@users.noreply.github.com>
2019-12-19 12:32:23 +0000

    Add files via upload

--- Proj2/farmacias_1.pl
+++ Proj2/farmacias_1.pl
@@ -1,20 +1,21 @@
 % Farmacias 1
 
-%farmacia(+Name, +Id, +Volume_Encomenda).
-farmacia("Sede", 0, 0).
-farmacia("Farm1", 1, 20).
-farmacia("Farm2", 2, 20).
-farmacia("Farm3", 3, 20).
-farmacia("Farm4", 4, 20).
-farmacia("Farm5", 5, 20).
+%farmacia(+Name, +Id, +Volume_Encomenda, +HoraInicio, +HoraFim).
+farmacia("Sede", 0, 0, 10, 22).
+farmacia("Farm1", 1, 10, 10, 22).
+farmacia("Farm2", 2, 40, 10, 22).
+farmacia("Farm3", 3, 25, 10, 22).
+farmacia("Farm4", 4, 15, 10, 22).
+farmacia("Farm5", 5, 5, 10, 22).
 
 %dist(+Id1, +Id2, +Time_Dist).
-dist(0, 1, 10).
-dist(0, 2, 20).
-dist(1, 2, 40).
-dist(1, 4, 45).
-dist(1, 5, 60).
-dist(2, 3, 30).
-dist(3, 4, 55).
-dist(3, 5, 20).
-dist(4, 5, 10).
+
+
+dist = [
+    [ 0, 20, 15, 30, 47, 28],
+    [20,  0,  2, 32, 23, 43],
+    [15,  2,  0,  3, 35, 26],
+    [30, 32,  3,  0, 21, 60],
+    [47, 23, 35, 21,  0, 42],
+    [28, 43, 26, 60, 42,  0]
+].
