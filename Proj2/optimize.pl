9b36cbdf9874794c4a95fd9a3fe11db4aa671beb
UnlimitedSpeed <55701019+UnlimitedSpeed@users.noreply.github.com>
2020-01-04 20:26:15 +0000

    Add files via upload

--- Proj2/optimize.pl
+++ Proj2/optimize.pl
@@ -5,27 +5,6 @@
 :-abolish(distTime/1).
 
 
-
-%farmacia(+Name, +Id, +Volume_Encomenda, +HoraInicio, +HoraFim).
-%farmacia("Sede", 0, 0, 10, 22).
-%farmacia("Farm1", 1, 10, 10, 22).
-%farmacia("Farm2", 2, 40, 10, 22).
-%farmacia("Farm3", 3, 25, 10, 22).
-%farmacia("Farm4", 4, 15, 10, 22).
-%farmacia("Farm5", 5, 5, 10, 22).
-%
-%%dist(+Id1, +Id2, +Time_Dist).
-%
-%
-%distTime([
-%     0, 20, 15, 30, 47, 28,
-%    20,  0,  2, 32, 23, 43,
-%    15,  2,  0,  3, 35, 26,
-%    30, 32,  3,  0, 21, 60,
-%    47, 23, 35, 21,  0, 42,
-%    28, 43, 26, 60, 42,  0
-%]).
-
 diff:-    
     write('Welcome to DIFF optimization program.'), nl,
     write('Select problem dimension:'), nl,
@@ -38,111 +17,217 @@ diff:-
     (Dimension = 2 ->
     consult('farmacias_2.pl');
     consult('farmacias_3.pl'))),
-    write(' 1-> Minimize Vans'), nl,
-    write(' 2-> Minimize Distance'), nl,
-    read(Option),
-    Option = 1, !,
-    minDist(1,Farmacies, D).
-diff:-
-    Option = 2,
-    write(' 1-> 1 Van'), nl,
-    write(' 3-> 3 Vans'), nl,
-    write(' 5-> 5 Vans'), nl,
+    write('Insert number of vans to be used:'), nl,
     read(NVans),
-    optMinDist(NVans, Farmacies, D).
+	getFarmacies(NFarm),
+	%optMinDist(NVans, _, _).
+	(NVans >= NFarm -> minDist(NFarm); (NVans = 1 -> minDist(1); minDist(NVans))).
 
-
-
-
-mult_dist(NVans, Farmacies, D):-
-    getFarmacies(N),
-    length(Farmacies, N),
-    domain(Farmacies, 0, N),
-    
-    
-    calc_min(Farmacies, D).
-
-calc_min([] , 30).
-calc_min([A, B|T], D).
-
-optMinDist(NVans, Farmacies, D):-
-    NVans = 1,!,
-    minDist(1, Farmacies, D).
-
-
-optMinDist(NVans, Farmacies, End):-
-    NVans = 3,!,
-	distTime(BigList),
-    % getFarmacies(N),
-    % N = 6, !,
-    % length(Farmacies, N),
-    % domain(Farmacies, 0, N),
-    Ss = [S1, S2, S3, S4, S5],
-    Es = [E1, E2, E3, E4, E5],
-    
-    element(2, BigList, A),
-    element(3, BigList, B),
-    element(4, BigList, C),
-    element(5, BigList, D),
-    element(6, BigList, E),
-    element(9, BigList, F),
-    element(10, BigList, G),
-    element(11, BigList, H),
-    element(12, BigList, I),
-    element(16, BigList, J),
-    element(17, BigList, K),
-    element(18, BigList, L),
-    element(23, BigList, M),
-    element(24, BigList, O),
-    element(30, BigList, P),
-    % AA #= A + 30,
-    % BB #= B + 30,
-    % CC #= C + 30,
-    % DD #= D + 30,
-    % EE #= E + 30,
-
-    Tasks=[
-        task(S1, A, E1, 1, 1),
-        %task(S1, B, E1, 1, 1),
-        %task(S1, C, E1, 1, 1),
-        %task(S1, D, E1, 1, 2),
-        %task(S1, E, E1, 1, 1),
-        task(S2, F, E2, 1, 1),
-        %task(S2, G, E2, 1, 1),
-        %task(S2, H, E3, 1, 2),
-        %task(S2, I, E4, 1, 2),
-        task(S3, J, E3, 1, 1),
-        %task(S3, K, E1, 1, 1),
-        %task(S3, L, E2, 1, 1),
-        %task(S4, M, E3, 1, 1),
-        task(S4, O, E4, 1, 1),
-        task(S5, P, E5, 1, 1)
-    ],
-    Machines = [machine(1, 1), machine(2, 1)],
-    domain(Ss, 1, 720),
-    maximum(End, Es),
-    cumulatives(Tasks, Machines, [bound(upper)]),
-    labeling([minimize(End)], Ss), write(End), write(Ss).
-
-
-
-
-% Cenas a optimizar: nº carrinhas; dist/carrinha
 
 % Minimizar distancia percorrida
-minDist(NVans, Farmacies, D) :-
+minDist(1) :-
 	distTime(BigList),
 	getFarmacies(N),
     length(Farmacies, N),
     N1 is N-1,
 	domain(Farmacies, 0, N1),
 	all_distinct(Farmacies),
-
 	element(1, Farmacies, 0),
-
 	getTotalDist(BigList, Farmacies, D, N),
+	labeling([minimize(D)], Farmacies), write(Farmacies), write(' - '), write(D).
 
-	labeling([minimize(D)], Farmacies), write(Farmacies), write(' - '), write(D), nl, fail.
+
+minDist(NVans) :-
+	getFarmacies(NFarm),
+	NFarm = 6,
+	distTime(BigList),
+
+	List = [L1, L2, L3, L4, L5],
+	domain(List, 1, NVans),
+
+	Ss = [S1, S2, S3, S4, S5],
+	Es = [E1, E2, E3, E4, E5],
+    
+	element(2, BigList, A),
+    element(3, BigList, B),
+    element(4, BigList, C),
+    element(5, BigList, D),
+    element(6, BigList, E),
+
+    AA #= A + A + 30,
+    BB #= B + B + 30,
+    CC #= C + C + 30,
+    DD #= D + D + 30,
+    EE #= E + E + 30,
+
+	Tasks = [
+		task(S1, AA, E1, 1, L1),
+		task(S2, BB, E2, 1, L2),
+		task(S3, CC, E3, 1, L3),
+		task(S4, DD, E4, 1, L4),
+		task(S5, EE, E5, 1, L5)
+	],
+	
+	getMachines(NVans, 1, [], Machines),
+
+	domain(Ss, 0, 720),
+    maximum(End, Es),
+	End #=< 720,
+
+	
+
+	cumulatives(Tasks, Machines, [bound(upper)]),
+	labeling([ff], List),
+    labeling([minimize(End)], Ss), write(End), write(Ss), write(List), fail.
+
+
+
+
+
+minDist(NVans) :-
+	getFarmacies(NFarm),
+	NFarm = 11,
+	distTime(BigList),
+
+	List = [L1, L2, L3, L4, L5, L6, L7, L8, L9, L10],
+	domain(List, 1, NVans),
+
+	Ss = [S1, S2, S3, S4, S5, S6, S7, S8, S9, S10],
+    Es = [E1, E2, E3, E4, E5, E6, E7, E8, E9, E10],
+
+	element(2, BigList, A),
+    element(3, BigList, B),
+    element(4, BigList, C),
+    element(5, BigList, D),
+    element(6, BigList, E),
+    element(7, BigList, F),
+    element(8, BigList, G),
+    element(9, BigList, H),
+    element(10, BigList, I),
+    element(11, BigList, J),
+
+    AA #= A + A + 30,
+    BB #= B + B + 30,
+    CC #= C + C + 30,
+    DD #= D + D + 30,
+    EE #= E + E + 30,
+	FF #= F + F + 30,
+	GG #= G + G + 30,
+	HH #= H + H + 30,
+	II #= I + I + 30,
+	JJ #= J + J + 30,
+
+	Tasks = [
+		task(S1, AA, E1, 1, L1),
+		task(S2, BB, E2, 1, L2),
+		task(S3, CC, E3, 1, L3),
+		task(S4, DD, E4, 1, L4),
+		task(S5, EE, E5, 1, L5),
+		task(S6, FF, E6, 1, L6),
+		task(S7, GG, E7, 1, L7),
+		task(S8, HH, E8, 1, L8),
+		task(S9, II, E9, 1, L9),
+		task(S10, JJ, E10, 1, L10)
+	],
+	
+	getMachines(NVans, 1, [], Machines),
+
+	domain(Ss, 0, 720),
+    maximum(End, Es),
+	End #=< 720,
+
+	cumulatives(Tasks, Machines, [bound(upper)]),
+	labeling([], List),
+    labeling([], Ss), write(End), write(Ss).
+
+
+
+
+minDist(NVans) :-
+	getFarmacies(NFarm),
+	NFarm = 16,
+	distTime(BigList),
+
+	List = [L1, L2, L3, L4, L5, L6, L7, L8, L9, L10, L11, L12, L13, L14, L15],
+	domain(List, 1, NVans),
+
+	Ss = [S1, S2, S3, S4, S5, S6, S7, S8, S9, S10, S11, S12, S13, S14, S15],
+    Es = [E1, E2, E3, E4, E5, E6, E7, E8, E9, E10, E11, E12, E13, E14, E15],
+
+	element(2, BigList, A),
+    element(3, BigList, B),
+    element(4, BigList, C),
+    element(5, BigList, D),
+    element(6, BigList, E),
+    element(7, BigList, F),
+    element(8, BigList, G),
+    element(9, BigList, H),
+    element(10, BigList, I),
+    element(11, BigList, J),
+    element(12, BigList, K),
+    element(13, BigList, L),
+    element(14, BigList, M),
+    element(15, BigList, P),
+    element(16, BigList, O),
+
+    AA #= A + A + 30,
+    BB #= B + B + 30,
+    CC #= C + C + 30,
+    DD #= D + D + 30,
+    EE #= E + E + 30,
+	FF #= F + F + 30,
+	GG #= G + G + 30,
+	HH #= H + H + 30,
+	II #= I + I + 30,
+	JJ #= J + J + 30,
+	KK #= K + K + 30,
+	LL #= L + L + 30,
+	MM #= M + M + 30,
+	PP #= P + P + 30,
+	OO #= O + O + 30,
+
+	Tasks = [
+		task(S1, AA, E1, 1, L1),
+		task(S2, BB, E2, 1, L2),
+		task(S3, CC, E3, 1, L3),
+		task(S4, DD, E4, 1, L4),
+		task(S5, EE, E5, 1, L5),
+		task(S6, FF, E6, 1, L6),
+		task(S7, GG, E7, 1, L7),
+		task(S8, HH, E8, 1, L8),
+		task(S9, II, E9, 1, L9),
+		task(S10, JJ, E10, 1, L10),
+		task(S11, KK, E11, 1, L11),
+		task(S12, LL, E12, 1, L12),
+		task(S13, MM, E13, 1, L13),
+		task(S14, PP, E14, 1, L14),
+		task(S15, OO, E15, 1, L15)
+	],
+
+	getMachines(NVans, 1, [], Machines),
+	write(Machines), nl,
+	domain(Ss, 0, 720),
+    maximum(End, Es),
+	End #=< 720,
+
+	labeling([], List),
+
+    cumulatives(Tasks, Machines, [bound(upper)]),
+    labeling([], Ss), write(End), write(Ss).
+
+
+
+
+
+getMachines(NVans, TempVans, TempMac, Machines) :-
+	NVans = TempVans,
+	append(TempMac, [machine(TempVans, 1)], Machines).
+getMachines(NVans, TempVans, TempMac, Machines) :-
+	append(TempMac, [machine(TempVans, 1)], TempMac1),
+	TempVans1 is TempVans + 1,
+	getMachines(NVans, TempVans1, TempMac1, Machines).
+
+
 
 
 
